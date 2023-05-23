f398f05b46bd24f8006c75e14e8bedf72866b262
Abhishek Jadhav <79781388+AbhishekJadhav2002@users.noreply.github.com>
2023-05-23 13:24:38 +0530

    Update users.validations.mjs

--- services/validations/users/users.validations.mjs
+++ services/validations/users/users.validations.mjs
@@ -5,8 +5,8 @@ function insertUserValidations() {
         body('name').trim().escape().isLength({ min: 2, max: 50 }).withMessage('Name length required is [2, 50]').isAlphanumeric('en-US', { ignore: ' @,-_.()\'' }).withMessage('Name is required'),
         body('email').trim().escape().isEmail().normalizeEmail({ gmail_remove_dots: false }).withMessage('Invalid email'),
         body('password').trim().escape().isLength({ min: 8, max: 50 }).withMessage('Password length required is [8, 50]'),
-        body('permissionLevel').trim().escape().isInt({ min: 1, max: 20 }).withMessage('Invalid permission level'),
-        body('avatar').trim().escape().isURL().withMessage('Invalid avatar url'),
+        //body('permissionLevel').trim().escape().isInt({ min: 1, max: 20 }).withMessage('Invalid permission level'),
+        //body('avatar').trim().escape().isURL().withMessage('Invalid avatar url'),
     ]
 }
 
